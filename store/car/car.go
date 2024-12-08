f444c52cddf3660a286f59cd2a110ae1c6ad2c5d
Sumukha <74605929+winterscar922@users.noreply.github.com>
2024-12-08 21:41:18 +0530

    added a few data store methods

--- store/car/car.go
+++ store/car/car.go
@@ -1,1 +1,93 @@
 package car
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/winterscar922/carZone/models"
+	engineDataStore "github.com/winterscar922/carZone/store/engine"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func open(db *sql.DB) Store {
+	return Store{db: db}
+}
+
+func (s Store) InsertCar(ctx context.Context, carReq models.CarRequest) (models.Car, error) {
+	var car models.Car
+
+	// verify if engine id is present or not in engine table
+	var engine_id = carReq.Engine.EngineId
+
+	if engine_id != uuid.Nil {
+		engineStore := engineDataStore.Store{Db: s.db}
+		exists, err := engineStore.CheckEngineById(ctx, engine_id)
+		if err != nil {
+			return models.Car{}, err
+		}
+		if !exists {
+			return models.Car{}, fmt.Errorf("no engine found with id %d", engine_id)
+		}
+	}
+
+	query := `insert into car (id, name, year, brand, fuel_type, engine_id, price, created_at, modified_at) 
+	values ($1,$2,$3,$4,$5,$6,$7,$8,$9) 
+	returning id, name, year, brand, fuel_type, engine_id, price, created_at, modified_at`
+
+	err := s.db.QueryRowContext(ctx, query,
+		uuid.New(),
+		carReq.Name,
+		carReq.Year,
+		carReq.Brand,
+		carReq.FuelType,
+		engine_id,
+		carReq.Price,
+		time.Now(),
+		time.Now()).Scan(
+		&car.CarId,
+		&car.Name,
+		&car.Year,
+		&car.Brand,
+		&car.FuelType,
+		&car.Engine.EngineId,
+		&car.Price,
+		&car.CreatedAt,
+		&car.ModifiedAt,
+	)
+
+	if err != nil {
+		return models.Car{}, errors.New("error while inserting car")
+	}
+	return car, nil
+}
+
+func (s Store) UpdateCar(ctx context.Context, carReq models.CarRequest) (models.Car, error) {
+	return models.Car{}, nil
+}
+
+func (s Store) DeleteCar(ctx context.Context, id int) error {
+	return nil
+}
+
+func (s Store) GetCarById(ctx context.Context, id int) (models.Car, error) {
+	var car models.Car
+	query := `select * from car where id = $1`
+
+	err := s.db.QueryRowContext(ctx, query, id).Scan(&car)
+
+	if err != nil {
+		return models.Car{}, errors.New(fmt.Sprintf("error while fetching car with id - %d", id))
+	}
+	return car, nil
+}
+
+func (s Store) GetCarByBrand(ctx context.Context, brand string) ([]models.Car, error) {
+	return []models.Car{}, nil
+}
--- store/engine/engine.go
+++ store/engine/engine.go
@@ -1,1 +1,45 @@
 package engine
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/winterscar922/carZone/models"
+)
+
+type Store struct {
+	Db *sql.DB
+}
+
+func open(db *sql.DB) Store {
+	return Store{Db: db}
+}
+
+func (s Store) GetEngineById(ctx context.Context, id uuid.UUID) (models.Engine, error) {
+	var engine models.Engine
+	query := `select * from engine where engine_id = $1`
+	err := s.Db.QueryRowContext(ctx, query, id).Scan(&engine)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return models.Engine{}, fmt.Errorf("no engine found with id %d", id)
+		}
+		return models.Engine{}, fmt.Errorf("error fetching engine with id %d: %w", id, err)
+	}
+
+	return engine, nil
+}
+
+func (s Store) CheckEngineById(ctx context.Context, id uuid.UUID) (bool, error) {
+	var exists bool
+	query := `select 1 from engine where engine_id = $1 limit 1`
+	err := s.Db.QueryRowContext(ctx, query, id).Scan(&exists)
+
+	if err != nil {
+		return false, fmt.Errorf("error fetching engine with id %d: %w", id, err)
+	}
+
+	return exists, nil
+}
